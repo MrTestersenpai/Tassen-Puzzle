9e00007ebc787709016d0fd0d75edbeda9811405
MrTestersenpai <139865309+MrTestersenpai@users.noreply.github.com>
2023-08-17 00:59:22 +0600

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -1,4 +1,3 @@
-
 var rows = 3;
 var columns = 3;
 
@@ -7,83 +6,59 @@ var otherTile; //blank tile
 
 var turns = 0;
 
-// var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
 
 window.onload = function() {
-    for (let r=0; r < rows; r++) {
-        for (let c=0; c < columns; c++) {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      let tile = document.createElement("img");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.src = imgOrder.shift() + ".jpg";
 
-            //<img id="0-0" src="1.jpg">
-            let tile = document.createElement("img");
-            tile.id = r.toString() + "-" + c.toString();
-            tile.src = imgOrder.shift() + ".jpg";
+      tile.addEventListener("touchstart", touchStart);
+      tile.addEventListener("touchmove", touchMove);
+      tile.addEventListener("touchend", touchEnd);
 
-            //DRAG FUNCTIONALITY
-            tile.addEventListener("dragstart", dragStart);  //click an image to drag
-            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
-            tile.addEventListener("dragenter", dragEnter);  //dragging image onto another one
-            tile.addEventListener("dragleave", dragLeave);  //dragged image leaving anohter image
-            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
-            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
-
-            document.getElementById("board").append(tile);
-
-        }
+      document.getElementById("board").append(tile);
     }
+  }
 }
 
-function dragStart() {
-    currTile = this; //this refers to the img tile being dragged
+function touchStart(e) {
+  e.preventDefault();
+  currTile = this;
 }
 
-function dragOver(e) {
-    e.preventDefault();
+function touchMove(e) {
+  e.preventDefault();
 }
 
-function dragEnter(e) {
-    e.preventDefault();
-}
+function touchEnd() {
+  otherTile = this;
 
-function dragLeave() {
+  let currCoords = currTile.id.split("-");
+  let r = parseInt(currCoords[0]);
+  let c = parseInt(currCoords[1]);
 
-}
+  let otherCoords = otherTile.id.split("-");
+  let r2 = parseInt(otherCoords[0]);
+  let c2 = parseInt(otherCoords[1]);
 
-function dragDrop() {
-    otherTile = this; //this refers to the img tile being dropped on
-}
+  let moveLeft = r == r2 && c2 == c - 1;
+  let moveRight = r == r2 && c2 == c + 1;
+  let moveUp = c == c2 && r2 == r - 1;
+  let moveDown = c == c2 && r2 == r + 1;
 
-function dragEnd() {
-    if (!otherTile.src.includes("3.jpg")) {
-        return;
-    }
+  let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
 
-    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
-    let r = parseInt(currCoords[0]);
-    let c = parseInt(currCoords[1]);
+  if (isAdjacent) {
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
 
-    let otherCoords = otherTile.id.split("-");
-    let r2 = parseInt(otherCoords[0]);
-    let c2 = parseInt(otherCoords[1]);
+    currTile.src = otherImg;
+    otherTile.src = currImg;
 
-    let moveLeft = r == r2 && c2 == c-1;
-    let moveRight = r == r2 && c2 == c+1;
-
-    let moveUp = c == c2 && r2 == r-1;
-    let moveDown = c == c2 && r2 == r+1;
-
-    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-    if (isAdjacent) {
-        let currImg = currTile.src;
-        let otherImg = otherTile.src;
-
-        currTile.src = otherImg;
-        otherTile.src = currImg;
-
-        turns += 1;
-        document.getElementById("turns").innerText = turns;
-    }
-
-
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+  }
 }
